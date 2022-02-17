336f1185ea4605ba695720165d7a4e0bbc318257
Tatenda. K. Tambo <tatendatambo@gmail.com>
2022-02-17 18:06:31 +0200

    Added setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@
 
 from distutils.core import setup
 
-setup(name='pickle_storage',
+setup(name='pickle-storage',
       version='0.1',
       description='Python Pickle-Based Data Storage',
       author='Tatenda Tambo',
       author_email='tatendatambo@gmail.com',
-      packages=['config'],
       )
