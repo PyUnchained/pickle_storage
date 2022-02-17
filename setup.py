17ae7f4cd09e6b7adf15476f9425ffba051dbb3e
Tatenda. K. Tambo <tatendatambo@gmail.com>
2022-02-17 17:56:29 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Python Pickle-Based Data Storage',
+      author='Tatenda Tambo',
+      author_email='tatendatambo@gmail.com',
+      packages=['distutils', 'distutils.command', 'config'],
+      )
